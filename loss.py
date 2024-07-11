28243ba0a128136017d230019aed2a7583edf50b
Apurva Mishra <apurvagyan@gmail.com>
2024-07-10 20:00:43 -0700

    implement cross entropy loss

--- loss.py
+++ loss.py
@@ -20,12 +20,14 @@ class MSE(Loss):
         '''
         Calculate the Mean Squared Error (MSE) loss.
         '''
+
         return np.mean((predicted.data - actual.data) ** 2)
     
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
         '''
         Calculate the gradient of the MSE loss.
         '''
+
         return Tensor(2 * (predicted.data - actual.data) / actual.data.size)
     
 '''
@@ -36,12 +38,36 @@ class MAE(Loss):
         '''
         Calculate the Mean Absolute Error (MAE) loss.
         '''
+
         return np.mean(np.abs(predicted.data - actual.data))
     
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
         '''
         Calculate the gradient of the MAE loss.
         '''
+
         grad = np.where(predicted.data > actual.data, 1, -1)
         return Tensor(grad / actual.data.size)
+    
+class CrossEntropy(Loss):
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        '''
+        Calculate the Cross-Entropy loss.
+        '''
+        epsilon = 1e-12
+        predicted_data = np.clip(predicted.data, epsilon, 1. - epsilon)
+
+        N = predicted_data.shape[0]
+        ce_loss = -np.sum(actual.data * np.log(predicted_data + 1e-9)) / N
+
+        return ce_loss
+    
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        '''
+        Calculate the gradient of the Cross-Entropy loss.
+        '''
+        epsilon = 1e-12
+        predicted_data = np.clip(predicted.data, epsilon, 1. - epsilon)
+        
+        return Tensor(-actual.data / predicted_data)
     
